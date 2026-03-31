6ee90c7d8b1f0c14e920035b9f54ff11f2c2e652
Yusuf Ali <129257836+Yusuf-0766@users.noreply.github.com>
2026-03-31 21:11:12 +0600

    Create Student.java

---
+++ Student.java
@@ -0,0 +1,15 @@
+public class Student {
+    int id;
+    String name;
+    double gpa;
+
+    public Student(int id, String name, double gpa) {
+        this.id = id;
+        this.name = name;
+        this.gpa = gpa;
+    }
+
+    public String toString() {
+        return id + "," + name + "," + gpa;
+    }
+}
